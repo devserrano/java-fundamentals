73c669b61143a33f30e19f3b7a813c0d57a881b9
Diego <diegoserr091111@gmail.com>
2026-06-10 12:15:02 -0600

    Initial repository structure

---
+++ VarJava/src/Variables.java
@@ -0,0 +1,11 @@
+
+
+public class Variables {
+    //Agregamos el metodo principal como main "psvm"
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+    }
+
+
+}
