9e76e4192d5e2d38809d04535e46100669a7a9cc
Diego <diegoserr091111@gmail.com>
2026-06-10 19:54:30 -0600

    Reto01 Presentacion personal

---
+++ 01-fundamentos/Fundamentos/src/retos/reto01/PresentacionPersonal.java
@@ -0,0 +1,9 @@
+package retos.reto01;
+
+public class PresentacionPersonal {
+    public static void main(String[] args) {
+        System.out.println("Diego Alberto Serrano Garcia");
+        System.out.println("20 años de edad");
+        System.out.println("Ingeniero en software");
+    }
+}
